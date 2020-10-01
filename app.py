66d818116a8314f284d36dbbe13cf150a8d77c2a
alimahatma <laluallistilo@gmail.com>
2020-10-01 21:57:40 +0700

    latihan redirect dan url for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 #secreat key
@@ -6,53 +6,23 @@ app.config["SECRET_KEY"] = "iniSecretkeyku"
 
 @app.route("/")
 def index():
-    #Data yg akan di looping dan dikondisikan
-    prodi = ['Sistem Informasi', 'Teknik Informatika','Bisnis Digital']
+    return render_template("index.html")
 
-    jurusan="Sistem Informasi"
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
-    return render_template("index.html", prodi=prodi, jurusan=jurusan)
-
-#layout
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-@app.route("/about") 
-def about():
-    return render_template("about.html")
+@app.route("/redirect-about")
+def redirect_about():
+    return redirect(url_for('about'))
 
-#parsing nilai int, string
-@app.route("/parsing/<string:nilaiku>")
-def ayo_parsing(nilaiku):
-    return "nilainya adalah : {} ".format(nilaiku)
-#argument parser
-@app.route("/parserargument")
-def ini_argument():
-    data = request.args.get("nilai")
-    return "nilainya dari argument parser : {}".format(data)
-
-#memparsing nilai dari url untuk set nilai session
-@app.route("/halaman/<int:nilai>")
-def session_pertama(nilai):
-    session["nilaiku"] = nilai
-    return "Berhasil mengeset nilainya"
-
-#menampilkan pada halaman lain
-@app.route("/halaman/lihat")
-def vew_session_pertama():
-    #teknik mengatasi error
-    try:
-        data = session["nilaiku"]
-        return "Nilai yang telah diset adalah = {} ".format(data)
-    except:
-        return "Anda Keluar, anda tiak memiliki akses.!"
-
-#logout / distroy session
-@app.route("/halaman/logout")
-def logout_session_pertama():
-    session.pop("nilaiku")
-    return "Berhasil logout / menghapus session"
+@app.route("/redirect-contact")
+def redirect_contact():
+    return redirect(url_for('contact'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
